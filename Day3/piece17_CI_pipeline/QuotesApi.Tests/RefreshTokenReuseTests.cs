b3a35fc49e69ddf5f96ac6547b08a34703c9fef7
Yash Rathi <yashsunilrathi@gmail.com>
2026-05-21 17:24:09 +0530

    Fix CI: isolate test DB via ConfigureServices

--- Day3/piece17_CI_pipeline/QuotesApi.Tests/RefreshTokenReuseTests.cs
+++ Day3/piece17_CI_pipeline/QuotesApi.Tests/RefreshTokenReuseTests.cs
@@ -3,7 +3,9 @@ using System.Net.Http.Json;
 using FluentAssertions;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using QuotesApi.Data;
 using QuotesApi.Dtos;
 using Xunit;
 
@@ -20,11 +22,16 @@ internal sealed class ApiFixture : WebApplicationFactory<Program>
 
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
-        builder.ConfigureAppConfiguration((_, cfg) =>
-            cfg.AddInMemoryCollection(new Dictionary<string, string?>
-            {
-                ["ConnectionStrings:DefaultConnection"] = $"Data Source={_dbPath}"
-            }));
+        builder.ConfigureServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(
+                d => d.ServiceType == typeof(DbContextOptions<AppDbContext>));
+            if (descriptor != null)
+                services.Remove(descriptor);
+
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlite($"Data Source={_dbPath}"));
+        });
     }
 
     protected override void Dispose(bool disposing)
