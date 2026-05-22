3141e1de67e9fe9144895b69bfb6ba7a330c3c4f
Yash Rathi <yashsunilrathi@gmail.com>
2026-05-22 12:45:12 +0530

    Fix coverage: exclude EF migrations from coverage, revert broken -- CLI arg

--- Day4/piece22_WireCI/Migrations/20260521085358_AddQuoteOwnerId.cs
+++ Day4/piece22_WireCI/Migrations/20260521085358_AddQuoteOwnerId.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
@@ -5,6 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace QuotesApi.Migrations
 {
     /// <inheritdoc />
+    [ExcludeFromCodeCoverage]
     public partial class AddQuoteOwnerId : Migration
     {
         /// <inheritdoc />
