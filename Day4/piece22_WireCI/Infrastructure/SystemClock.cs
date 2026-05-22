3e3d37309018b62163c755e42a7c1a3fa413ba4d
Yash Rathi <yashsunilrathi@gmail.com>
2026-05-22 12:36:52 +0530

    Fix coverage: exclude Migrations, add ExceptionMiddleware test, exclude SystemClock

--- Day4/piece22_WireCI/Infrastructure/SystemClock.cs
+++ Day4/piece22_WireCI/Infrastructure/SystemClock.cs
@@ -1,7 +1,9 @@
+using System.Diagnostics.CodeAnalysis;
 using QuotesApi.Abstractions;
 
 namespace QuotesApi.Infrastructure;
 
+[ExcludeFromCodeCoverage]
 public class SystemClock : IClock
 {
     public DateTimeOffset UtcNow => DateTimeOffset.UtcNow;
